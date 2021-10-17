7e108d4ff9e3130a3f32f08d34451ddba1a9745d
c0024036 <c0024036@newcastle.ac.uk>
2021-10-17 13:43:56 +0100

    Only adds active app to tasks.json if first time

--- main.py
+++ main.py
@@ -24,6 +24,16 @@ def write_json(data, filename):
         json.dump(data, f, indent=4)
 
 
+def is_new_task(app):
+    with open('tasks.json', "r") as file:
+        data = json.load(file)
+        tasks = data["tasks"]
+        for task in tasks:
+            if task["name"] == app:
+                return False
+        return True
+
+
 if __name__ == '__main__':
     active_app_name = ""
     while True:
@@ -33,13 +43,14 @@ if __name__ == '__main__':
         if active_app_name != new_app_name:
             active_app_name = new_app_name
 
-            with open('tasks.json', "r") as file:
-                data = json.load(file)
-                temp = data["tasks"]
-                entry = {"name": active_app_name}
-                temp.append(entry)
+            if is_new_task(active_app_name):
+                with open('tasks.json', "r") as file:
+                    data = json.load(file)
+                    tasks = data["tasks"]
+                    entry = {"name": active_app_name}
+                    tasks.append(entry)
 
-            write_json(data, 'tasks.json')
+                write_json(data, 'tasks.json')
 
             print(active_app_name)
 
