2aaf6fca36cd49895ede4790494c8c5ca7a770b5
c0024036 <c0024036@newcastle.ac.uk>
2021-10-18 18:08:22 +0100

    Create class Task

--- task.py
+++ task.py
@@ -1,3 +1,9 @@
+class Task:
+    def __init__(self, name, time_entries):
+        self.name = name
+        self.time_entries = time_entries
+
+
 class TimeEntry:
     def __init__(self, start, end):
         self.start = start
